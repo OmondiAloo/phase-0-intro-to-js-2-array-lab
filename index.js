23022a8069c850088705cd6fafa0761fdcda7d6f
Emmanuel Omondi Aloo <aloo25emmanuel@gmail.com>
2022-06-04 16:22:50 +0300

    commit

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(Ralph){
+ cats.push("Ralph")
+ return cats
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob")
+    return cats
+   }
+   function destructivelyRemoveLastCat(Bob){
+    cats.pop()
+    return cats
+   }
+   function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+   }
+   function appendCat(name) {
+     return [...cats,name]
+      }
+
+      function prependCat(name) {
+        return [name, ...cats]
+         }
+
+         function removeLastCat() {
+            return cats.slice(0,cats.length-1);
+             }
+
+             function removeFirstCat() {
+                return cats.slice(1);
+                 }
+  
